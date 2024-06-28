eea0337c49a0d8bbfb5e655781652eb0f301ffda
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-06-27 20:44:06 -0400

    Flask Seed

--- server/seed.py
+++ server/seed.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 #server/seed.py
-
+from random import choice as rc
 from app import app
 from models import db, Pet
+from faker import Faker
 
 with app.app_context():
+
+    Pet.query.delete()
+
+    fake = Faker()
 
     # Create an empty list
     pets = []
 
-    # Add some Pet instances to the list
-    pets.append(Pet(name = "Fido", species = "Dog"))
-    pets.append(Pet(name = "Whiskers", species = "Cat"))
-    pets.append(Pet(name = "Hermie", species = "Hamster"))
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
 
+    # Add some Pet instances to the list
+    for n in range(10):
+        pet = Pet(name=fake.name(), species=rc(species))
+        pets.append(pet)
+    
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
 
